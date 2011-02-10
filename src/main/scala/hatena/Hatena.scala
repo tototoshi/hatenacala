c039d4b06acf8fd729d456f0a9227eaa2745e792
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-02-10 22:44:27 +0900

    引数チェック

--- src/main/scala/hatena/Hatena.scala
+++ src/main/scala/hatena/Hatena.scala
@@ -18,17 +18,19 @@ object Hatena {
         case "get"  => Draft.open(args(2))
         case "update"  => args.length match {
           case 4 => Draft.update(args(2), args(3))
-          case _ => error("Error: Invalid argument.")
+          case _ => println(usage)
         }
-        case _ => error("Error: Invalid argument.")
+        case _ => println(usage)
       }
+      case _ => println(usage)
     }
   }
 
   val usage ="""|usage:
                 |  hatena draft list
-                |  hatena draft add  filename
-                |  hatena draft save id""".stripMargin
+                |  hatena draft add 'filename'
+                |  hatena draft update 'id' 'filename'
+                |  hatena draft rm 'id'""".stripMargin
 
   val user: HatenaUser = new HatenaUser(HatenaProps.name, HatenaProps.password)
 }
