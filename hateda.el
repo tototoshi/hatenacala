237e70c225b15a2fc585535038d4820bc3988e27
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-02-11 20:03:53 +0900

    .elできた

--- hateda.el
+++ hateda.el
@@ -27,20 +27,112 @@
 ;;  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 ;;  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+(require 'cl)
+(require 'anything)
 
-(defvar hateda-exec-path nil)
-(setq hateda-exec-path "~/bin/hateda")
+(defvar hateda-exec-path "~/bin/hateda")
+(defvar hateda-draft-dir "~/hateda/")
+(defvar hateda-draft-entry-regexp "Draft id: \\([1-9][0-9]*\\)  title: \\(.*\\)  date: \\([0-9]+\\)")
+(defstruct hateda-draft-entry
+  id  title  date)
+
+(defvar hateda-draft-tmp-file "/tmp/hateda-draft")
+
+(defun hateda-char-replace (from-char to-char string)
+  (let ((chars (string-to-list string)))
+    (apply #'string
+           (loop for c in chars
+                 collect (if (= from-char c) to-char c)))))
+
+(defun hateda-delete-trailing-slash (string)
+   (cond ((= ?/ (first (reverse (string-to-list string))))
+          (substring string 0 (1- (length string))))
+         (t string)))
+
+(defun hateda-basename (abspath)
+  (first (reverse (split-string (hateda-delete-trailing-slash abspath) "/"))))
 
 (defun hateda-draft-add-this-buffer ()
   (interactive)
-  (hateda-draft-add-file (or (buffer-file-name))))
+  (write-region (point-min) (point-max) hateda-draft-tmp-file nil nil)
+  (message "Wait a moment.....")
+  (shell-command (hateda-mkString `(,hateda-exec-path "draft" "add" ,hateda-draft-tmp-file) " ")))
 
 (defun hateda-mkString (lst &optional spliter)
   (reduce (lambda (m o) (concat m (or spliter "") o)) lst))
 
-(defun hateda-draft-add-file (file)
-  (shell-command (hateda-mkString `(,hateda-exec-path "draft" "add" ,file) " ")))
+(defun hateda-lines-to-list (lines)
+  (split-string lines "\n" ))
 
+(defun hateda-draft-list ()
+  (shell-command-to-string (hateda-mkString `(,hateda-exec-path "draft" "list") " ")))
 
+(defun hateda-get-id-of-current-buffer ()
+  (let ((filename (hateda-basename (buffer-file-name))))
+    (cond ((string-match "\\([0-9]+\\).*" filename)
+           (match-string 1 filename))
+          (t ""))))
+
+(defun hateda-draft-update ()
+  (interactive)
+  (write-region (point-min) (point-max) hateda-draft-tmp-file nil nil)
+  (message "Wait a moment.....")
+  (shell-command
+   (hateda-mkString
+    `(,hateda-exec-path "draft" "update" ,(hateda-get-id-of-current-buffer) ,hateda-draft-tmp-file)
+    " ")))
+
+(defun hateda-string-to-draft-entry (string)
+  (when (string-match hateda-draft-entry-regexp string)
+    (make-hateda-draft-entry :id (match-string 1 string)
+                             :title (match-string 2 string)
+                             :date (match-string 3 string))))
+
+(defun hateda-draft-make-file-name (id title)
+  (interactive)
+  (concat
+   (hateda-delete-trailing-slash hateda-draft-dir)
+   "/" id "-" (hateda-char-replace ?/ ?_ title)))
+
+(defun hateda-draft-open-file (id title)
+  (unless (file-exists-p hateda-draft-dir)
+    (mkdir hateda-draft-dir))
+  (with-current-buffer (find-file (hateda-draft-make-file-name id title))
+    (erase-buffer)
+    (shell-command (hateda-mkString `(,hateda-exec-path "draft" "get" ,id) " ") (current-buffer))))
+
+(defun anything-hateda-draft-open-file (description)
+  (message "Wait a moment....")
+  (let ((entry (hateda-string-to-draft-entry description)))
+    (hateda-draft-open-file (hateda-draft-entry-id entry) (hateda-draft-entry-title entry))))
+
+(defun anything-hateda-draft-rm (description)
+  (message "Wait a moment....")
+  (let ((entry (hateda-string-to-draft-entry description)))
+    (hateda-draft-rm (hateda-draft-entry-id entry))))
+
+(defun hateda-draft-rm (id)
+  (shell-command (hateda-mkString `(,hateda-exec-path "draft" "rm" ,id) " ")))
+
+(defun anything-hateda-draft-entries-get-candidates ()
+  (message "Wait a moment....")
+  (hateda-lines-to-list (hateda-draft-list)))
+
+(anything-hateda-draft-entries-get-candidates)
+
+(define-anything-type-attribute 'hateda
+  '((action ("Open" . anything-hateda-draft-open-file)
+            ("Delete" . anything-hateda-draft-rm))))
+
+(setq anything-c-source-hateda-draft-entries
+  '((name . "hatena-diary")
+    (candidates . anything-hateda-draft-entries-get-candidates)
+    (requires-pattern . 0)
+    (candidate-number-limit . 50)
+    (type . hateda)))
+
+(defun anything-hateda ()
+  (interactive)
+  (anything 'anything-c-source-hateda-draft-entries))
 
 
